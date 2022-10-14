29d74769762174c5d26aa01548f71270a876cf2b
pnguyen <9phuc.nguyen6@gmail.com>
2022-10-14 01:02:57 -0500

    moved archived files

--- archive/installer.sh
+++ archive/installer.sh
@@ -3,7 +3,6 @@
 # Setup
 set -e
 git_repo_url="https://raw.githubusercontent.com/Conphucious/SteamDeckSleeper/master"
-file_name="jsds.sh"
 path_name="JSDS"
 
 # Prompt for installation
@@ -18,15 +17,17 @@ if [ -d "$path_name" ]; then
     rm -R "$path_name"
 fi
 mkdir $path_name
-curl -o ~/Desktop/"$path_name/$file_name" "$git_repo_url/$file_name"
+curl -o ~/Desktop/"$path_name"/sleep-downloader.sh "$git_repo_url/sleep-downloader.sh"
+curl -o ~/Desktop/"$path_name"/core.sh "$git_repo_url/core.sh"
 
 # Permissions
-chmod 555 ~/Desktop/"$path_name/$file_name"
+chmod 555 ~/Desktop/"$path_name"/sleep-downloader.sh
+chmod 555 ~/Desktop/"$path_name"/core.sh
 
 # Finish
 zenity --question --width=500 --text="Do you want to add JSDS to your steam library?"
 if [ "$?" != 0 ]; then
     exit 1;
 fi
-steamos-add-to-steam ~/Desktop/$path/sleep-downloader.sh
+steamos-add-to-steam ~/Desktop/"$path_name"/sleep-downloader.sh
 zenity --info --width=500 --text="Installation complete for JSDS!"
--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@
 # Setup
 set -e
 git_repo_url="https://raw.githubusercontent.com/Conphucious/SteamDeckSleeper/master"
+file_name="jsds.sh"
 path_name="JSDS"
 
 # Prompt for installation
@@ -17,17 +18,15 @@ if [ -d "$path_name" ]; then
     rm -R "$path_name"
 fi
 mkdir $path_name
-curl -o ~/Desktop/"$path_name"/sleep-downloader.sh "$git_repo_url/sleep-downloader.sh"
-curl -o ~/Desktop/"$path_name"/core.sh "$git_repo_url/core.sh"
+curl -o ~/Desktop/"$path_name/$file_name" "$git_repo_url/$file_name"
 
 # Permissions
-chmod 555 ~/Desktop/"$path_name"/sleep-downloader.sh
-chmod 555 ~/Desktop/"$path_name"/core.sh
+chmod 555 ~/Desktop/"$path_name/$file_name"
 
 # Finish
 zenity --question --width=500 --text="Do you want to add JSDS to your steam library?"
 if [ "$?" != 0 ]; then
     exit 1;
 fi
-steamos-add-to-steam ~/Desktop/"$path_name"/sleep-downloader.sh
+steamos-add-to-steam ~/Desktop/$path/$file_name".sh
 zenity --info --width=500 --text="Installation complete for JSDS!"
