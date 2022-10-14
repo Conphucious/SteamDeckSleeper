573a7a87557d83d81849253b69bc8bc4b73766dd
pnguyen <9phuc.nguyen6@gmail.com>
2022-10-14 00:51:34 -0500

    two part bugfix on installer

---
+++ old/jsds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sleep_timer=10
+steam_dir="/home/deck/.local/share/Steam/steamapps/downloading"
+
+function is_steam_downloading() {
+    result=$(find "$steam_dir" -maxdepth 1 -type f)
+    if [ -z "${result}" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+zenity --info --width=500 --text="Process downloads"
+is_steam_downloading
+while [ $? == 1 ]; do
+    echo 'waiting for downloading to finish...'
+    sleep $sleep_timer
+    is_steam_downloading
+done
+zenity --info --width=500 --text="Finished downloads..."
